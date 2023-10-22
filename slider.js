2fcfd2f23dcb75e141656bc72fbf0b26d447c8af
Max Zabelenkov <maxx.zabelenkov@gmail.com>
2023-10-22 19:16:39 +0300

    collectin sectib + slider

---
+++ slider.js
@@ -0,0 +1,28 @@
+"use strict";
+
+(function () {
+    'use strict';
+    let breakpoint = window.matchMedia('(max-width:56.25rem)');
+    let swiperWrapper = document.getElementById("swiperWrapper");
+    let swiper;
+    let breakpointChecker = function breakpointChecker() {
+        if (breakpoint.matches === true) {
+            if (swiper !== undefined) swiper.destroy(true, true);
+            return;
+        } else if (breakpoint.matches === false) {
+            return enableSwiper();
+        }
+        
+    };
+    let enableSwiper = function enableSwiper() {
+        swiper = new Swiper('.swiper', {
+
+            slidesPerView: 4.5,
+            spaceBetween: 15,
+        });
+    };
+    breakpoint.addListener(breakpointChecker);
+    breakpointChecker();
+})();
+
+
